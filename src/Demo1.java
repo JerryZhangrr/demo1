d19bc63c5c69ff366ccf17d860240b901f5f20e7
JerryZhangrr <jerry1997zhang@163.com>
2019-03-29 11:03:12 +0800

    third modify

--- src/Demo1.java
+++ src/Demo1.java
@@ -17,27 +17,32 @@ public class Demo1 {
         //准备两个数字，让用户猜
         String lottery = "" + (int)(Math.random()*10) + (int)(Math.random()*10);
         //用户输入
-        System.out.print("请输入猜测的数字：");
-        Scanner input = new Scanner(System.in);
-        String guess = input.nextLine();
-        //切分系统随机准备的数字
-        char lotteryDigit1 = lottery.charAt(0);
-        char lotteryDigit2 = lottery.charAt(1);
-        char guess1 = guess.charAt(0);
-        char guess2 = guess.charAt(1);
-        //比较用户和彩票
-        if(guess.equals(lottery)){
-            System.out.println("恭喜你，精准猜中");
-        }
-        else if(guess1==lotteryDigit1&&guess2==lotteryDigit1){
-            System.out.println("恭喜你，中小奖了，顺序不一样");
-        }
-        else if(guess1 ==lotteryDigit1||guess1==lotteryDigit2||guess2==lotteryDigit2
-                ||guess2==lotteryDigit1){
-            System.out.println("恭喜你，只猜中了一个");
-        }
-        else{
-            System.out.println("很抱歉，你没有猜中");
+        for(int i = 0;i<3;i++) {
+            System.out.print("请输入猜测的数字：");
+            Scanner input = new Scanner(System.in);
+            String guess = input.nextLine();
+            //切分系统随机准备的数字
+            char lotteryDigit1 = lottery.charAt(0);
+            char lotteryDigit2 = lottery.charAt(1);
+            char guess1 = guess.charAt(0);
+            char guess2 = guess.charAt(1);
+            //比较用户和彩票
+            if (guess.equals(lottery)) {
+                System.out.println("恭喜你，精准猜中");
+                break;
+            }
+            else if (guess1 == lotteryDigit1 && guess2 == lotteryDigit1) {
+                System.out.println("恭喜你，中小奖了，顺序不一样");
+                break;
+            }
+            else if (guess1 == lotteryDigit1 || guess1 == lotteryDigit2 || guess2 == lotteryDigit2
+                    || guess2 == lotteryDigit1) {
+                System.out.println("恭喜你，只猜中了一个");
+                break;
+            }
+            else {
+                System.out.println("很抱歉，你没有猜中");
+            }
         }
 
 
